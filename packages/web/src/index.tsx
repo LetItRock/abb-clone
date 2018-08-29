475248b01b8098645ee3cab03b8fda2ddf56cd51
Pavlo Tymchuk <pawel.tymczuk@gmail.com>
2018-08-29 23:43:01 +0200

    - graphql apollo libraries and config for provider; - tslint config;

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -1,10 +1,14 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { ApolloProvider } from 'react-apollo';
 import App from './App';
+import { client } from './apollo';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <App />,
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
